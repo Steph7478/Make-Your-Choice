8147d1234137a1a303d742bd4af99bf541cac709
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-08-04 12:00:47 -0300

    feat: Dialog entity + test

---
+++ src/main/java/com/make_your_choice/domain/models/DialogEntity.java
@@ -0,0 +1,33 @@
+package com.make_your_choice.domain.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+
+@Entity
+@Access(AccessType.FIELD)
+public class DialogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    public DialogEntity() {
+    }
+
+    public String getCode() {
+        if (this.id == null) {
+            return null;
+        }
+        return "D" + this.id;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
---
+++ src/test/java/com/make_your_choice/domain/models/DialogEntityTest.java
@@ -0,0 +1,33 @@
+package com.make_your_choice.domain.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static com.make_your_choice.support.TestReflectionUtils.setField;
+
+public class DialogEntityTest {
+    @Test
+    void testGetCodeReturnsNullWhenIdIsNull() throws Exception {
+        DialogEntity dialog = new DialogEntity();
+        setField(dialog, "id", null);
+
+        assertNull(dialog.getCode());
+    }
+
+    @Test
+    void testGetCodeReturnsCorrectCode() throws Exception {
+        DialogEntity dialog = new DialogEntity();
+        setField(dialog, "id", 42L);
+
+        assertEquals("D42", dialog.getCode());
+    }
+
+    @Test
+    void testGetText() throws Exception {
+        DialogEntity dialog = new DialogEntity();
+        setField(dialog, "text", "Hello im the test!");
+
+        assertEquals("Hello im the test!", dialog.getText());
+    }
+}
