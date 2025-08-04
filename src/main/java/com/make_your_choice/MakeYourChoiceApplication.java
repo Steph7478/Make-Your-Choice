deee626f715cc503c56fd115b4e89eecbb4e82f4
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-08-04 09:41:35 -0300

    initial commit

---
+++ src/main/java/com/make_your_choice/MakeYourChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.make_your_choice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeYourChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeYourChoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/make_your_choice/MakeYourChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.make_your_choice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeYourChoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
