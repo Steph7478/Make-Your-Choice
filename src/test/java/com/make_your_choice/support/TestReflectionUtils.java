b6028982f4a9625cf2db8af7b605067af37e640d
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-08-25 11:42:18 -0300

    feat: comments in utils to remember

--- src/test/java/com/make_your_choice/support/TestReflectionUtils.java
+++ src/test/java/com/make_your_choice/support/TestReflectionUtils.java
@@ -2,8 +2,30 @@ package com.make_your_choice.support;
 
 import java.lang.reflect.Field;
 
+/**
+ * Utility class for manipulating object fields using Java Reflection.
+ *
+ *
+ * What is Reflection?
+ * Reflection is a feature in Java that allows programs to inspect and
+ * manipulate
+ * classes, methods, and fields at runtime, even if they are private or
+ * protected.
+ * This can be useful for testing, frameworks, or dynamic behavior, but should
+ * be used carefully since it breaks encapsulation.
+ *
+ */
+
 public class TestReflectionUtils {
 
+    /**
+     * Sets the value of a field in the given target object using Java Reflection.
+     * This method can access private or protected fields by forcing accessibility.
+     *
+     * @param target    the object whose field should be modified
+     * @param fieldName the name of the field to set
+     * @param value     the new value for the field
+     */
     public static void setField(Object target, String fieldName, Object value) {
         try {
             Field field = target.getClass().getDeclaredField(fieldName);
@@ -14,6 +36,14 @@ public class TestReflectionUtils {
         }
     }
 
+    /**
+     * Gets the value of a field in the given target object using Java Reflection.
+     * This method can access private or protected fields by forcing accessibility.
+     *
+     * @param target    the object whose field value should be retrieved
+     * @param fieldName the name of the field to get
+     * @return the current value of the field
+     */
     public static Object getField(Object target, String fieldName) {
         try {
             Field field = target.getClass().getDeclaredField(fieldName);
