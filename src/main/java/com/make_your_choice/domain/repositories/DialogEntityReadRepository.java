3795c52a63bfb43b7b76c9532731120b2aedd093
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-08-27 15:07:30 -0300

    feat: findChoicesByDialogCode interface

--- src/main/java/com/make_your_choice/domain/repositories/DialogEntityReadRepository.java
+++ src/main/java/com/make_your_choice/domain/repositories/DialogEntityReadRepository.java
@@ -3,6 +3,7 @@ package com.make_your_choice.domain.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import com.make_your_choice.domain.entities.ChoiceEntity;
 import com.make_your_choice.domain.entities.DialogEntity;
 
 public interface DialogEntityReadRepository {
@@ -10,5 +11,8 @@ public interface DialogEntityReadRepository {
 
     List<DialogEntity> findAllByCodes(List<String> codes);
 
+    List<ChoiceEntity> findChoicesByDialogCode(String dialogCode);
+
     Optional<DialogEntity> findByCode(String code);
+
 }
