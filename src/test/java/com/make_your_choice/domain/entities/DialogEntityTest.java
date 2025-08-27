9ac499e8ffe4ab6ecf182cbc20c94a653007d973
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-08-27 13:22:23 -0300

    feat: test for getChoices()

--- src/test/java/com/make_your_choice/domain/entities/DialogEntityTest.java
+++ src/test/java/com/make_your_choice/domain/entities/DialogEntityTest.java
@@ -2,6 +2,8 @@ package com.make_your_choice.domain.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import static com.make_your_choice.support.TestReflectionUtils.setField;
@@ -29,5 +31,23 @@ public class DialogEntityTest {
         setField(dialog, "dialog", "Hello im the dialog test!");
 
         assertEquals("Hello im the dialog test!", dialog.getDialog());
+    }
+
+    @Test
+    void testGetChoices() throws Exception {
+        ChoiceEntity choice1 = new ChoiceEntity();
+        setField(choice1, "choice", "Choice 1");
+
+        ChoiceEntity choice2 = new ChoiceEntity();
+        setField(choice2, "choice", "Choice 2");
+
+        DialogEntity dialog = new DialogEntity();
+        setField(dialog, "choices", List.of(choice1, choice2));
+
+        List<ChoiceEntity> results = dialog.getChoices();
+
+        assertEquals(2, results.size());
+        assertEquals("Choice 1", results.get(0).getChoice());
+        assertEquals("Choice 2", results.get(1).getChoice());
     }
 }
