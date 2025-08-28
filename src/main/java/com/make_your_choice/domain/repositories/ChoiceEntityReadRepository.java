383e8b3180fad2bbdf54c7b38846b232763a0c60
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-08-28 14:25:57 -0300

    feat: notes about jparepository

--- src/main/java/com/make_your_choice/domain/repositories/ChoiceEntityReadRepository.java
+++ src/main/java/com/make_your_choice/domain/repositories/ChoiceEntityReadRepository.java
@@ -6,13 +6,30 @@ import java.util.Optional;
 import com.make_your_choice.domain.entities.ChoiceEntity;
 
 public interface ChoiceEntityReadRepository {
-    List<ChoiceEntity> findAll();
+
+    // Note:
+    // If this interface extended JpaRepository (or another Spring Data repository),
+    // we could use @Query for quick custom queries without writing an
+    // implementation.
+    // Example:
+    //
+    // @Query("SELECT c FROM ChoiceEntity c WHERE c.code = :code")
+    // Optional<ChoiceEntity> findByCode(@Param("code") String code);
+    //
+    // However, since this interface does NOT extend JpaRepository, Spring Data
+    // will not process annotations here. The methods must be implemented
+    // manually in a concrete class (e.g., ChoiceRepositoryImpl).
+    // Example of extending JpaRepository:
+    // extends JpaRepository<ChoiceEntity, Long>
 
     Optional<ChoiceEntity> findByCode(String code);
-
-    List<ChoiceEntity> findAllByCodes(List<String> codes);
 
     Optional<ChoiceEntity> findByDialogCode(String dialogCode);
 
     Optional<ChoiceEntity> findByNextDialogCode(String nextDialogCode);
+
+    List<ChoiceEntity> findAll();
+
+    List<ChoiceEntity> findAllByCodes(List<String> codes);
+
 }
