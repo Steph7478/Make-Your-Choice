a5fb855742cee6d3d2c5d8e862b09c2a29b6d391
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-02 10:40:49 -0300

    refactor: /choices params switched

--- src/main/java/com/make_your_choice/presentation/controllers/DialogController.java
+++ src/main/java/com/make_your_choice/presentation/controllers/DialogController.java
@@ -18,35 +18,35 @@ import com.make_your_choice.domain.entities.ChoiceEntity;
 @RestController
 @RequestMapping("/dialog")
 public class DialogController {
-    private final GetDialogByCodeUseCase getDialogByCodeUseCase;
-    private final GetChoicesByDialogCodeUseCase getChoicesByDialogCodeUseCase;
+        private final GetDialogByCodeUseCase getDialogByCodeUseCase;
+        private final GetChoicesByDialogCodeUseCase getChoicesByDialogCodeUseCase;
 
-    public DialogController(GetDialogByCodeUseCase getDialogByCodeUseCase,
-            GetChoicesByDialogCodeUseCase getChoicesByDialogCodeUseCase) {
-        this.getChoicesByDialogCodeUseCase = getChoicesByDialogCodeUseCase;
-        this.getDialogByCodeUseCase = getDialogByCodeUseCase;
-    }
+        public DialogController(GetDialogByCodeUseCase getDialogByCodeUseCase,
+                        GetChoicesByDialogCodeUseCase getChoicesByDialogCodeUseCase) {
+                this.getChoicesByDialogCodeUseCase = getChoicesByDialogCodeUseCase;
+                this.getDialogByCodeUseCase = getDialogByCodeUseCase;
+        }
 
-    @GetMapping("/{dialogCode}")
-    public ChoiceEntity getChoiceByDialogCode(@PathVariable String dialogCode) {
-        return getDialogByCodeUseCase.execute(dialogCode)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Choice not found for dialog"));
-    }
+        @GetMapping("/{dialogCode}")
+        public ChoiceEntity getChoiceByDialogCode(@PathVariable String dialogCode) {
+                return getDialogByCodeUseCase.execute(dialogCode)
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                                "Choice not found for dialog"));
+        }
 
-    @GetMapping({ "/choices", "/choices/{code}" })
-    public List<ChoiceEntity> getChoicesByDialogCode(
-            @PathVariable(required = false) String code,
-            @RequestParam(required = false) String codeParam) {
+        @GetMapping({ "/choices", "/choices/{code}" })
+        public List<ChoiceEntity> getChoicesByDialogCode(
+                        @RequestParam(required = false) String codeParam, @PathVariable(required = false) String code) {
 
-        String finalCode = Optional.ofNullable(code)
-                .orElse(codeParam);
+                String finalCode = Optional.ofNullable(code)
+                                .orElse(codeParam);
 
-        return Optional.ofNullable(finalCode)
-                .filter(c -> !c.isEmpty())
-                .map(getChoicesByDialogCodeUseCase::execute)
-                .filter(list -> !list.isEmpty())
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "No choices found for this dialog code"));
-    }
+                return Optional.ofNullable(finalCode)
+                                .filter(c -> !c.isEmpty())
+                                .map(getChoicesByDialogCodeUseCase::execute)
+                                .filter(list -> !list.isEmpty())
+                                .orElseThrow(() -> new ResponseStatusException(
+                                                HttpStatus.NOT_FOUND, "No choices found for this dialog code"));
+        }
 
 }
