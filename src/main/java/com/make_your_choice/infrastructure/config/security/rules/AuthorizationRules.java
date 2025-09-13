4bc1fc8bb9b55d42185341fc56b1e0ea8e1cbffe
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-13 14:27:39 -0300

    feat: authorizationRules

---
+++ src/main/java/com/make_your_choice/infrastructure/config/security/rules/AuthorizationRules.java
@@ -0,0 +1,47 @@
+package com.make_your_choice.infrastructure.config.security.rules;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AuthorizationRules {
+
+    public static class EndpointRule {
+        private final HttpMethod method;
+        private final String path;
+        private final boolean requiresAuth;
+
+        public EndpointRule(HttpMethod method, String path, boolean requiresAuth) {
+            this.method = method;
+            this.path = path;
+            this.requiresAuth = requiresAuth;
+        }
+
+        public HttpMethod getMethod() {
+            return method;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public boolean requiresAuth() {
+            return requiresAuth;
+        }
+    }
+
+    public List<EndpointRule> publicEndpoints() {
+        return List.of(
+                new EndpointRule(HttpMethod.GET, "/choices/**", false),
+                new EndpointRule(HttpMethod.GET, "/dialogs/**", false));
+    }
+
+    public List<EndpointRule> privateEndpoints() {
+        return List.of(
+        // new EndpointRule(HttpMethod.POST, "/choices/**", true),
+        // new EndpointRule(HttpMethod.POST, "/dialogs/**", true)
+        );
+    }
+}
